1403ce9390c067b8c4bc778f926e0b413fbb46f6
whostmdz <thomasdiemoz@gmail.com>
2026-05-09 11:25:25 +0200

    bubble sort

--- src/algo/BubbleSort.java
+++ src/algo/BubbleSort.java
@@ -1,5 +1,24 @@
 package algo;
 
-public class BubbleSort {
+public class BubbleSort implements Sorter{
+        @Override
+    public void sort(int[] tab){
+        int d = tab.length;
+        while d > 0{
+            i=0;
+            while i < d-1{
+                if tab[i] > tab[i+1]{
+                    swap(tab, i, i+1);
+                }
+                i++;
+            }
+            d--;
+        }
+    }
+
+        @Override
+    public String nameAlgo(){
+        return "Bubble sort";
+    }
     
 }
